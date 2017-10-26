7e983da665474f2bff69262e3255c1d2c6c1a7cf
Pavel Korolev <dev@borodust.org>
2017-10-27 00:02:23 +0300

    Allow to register custom paths

--- utils.lisp
+++ utils.lisp
@@ -1,21 +1,25 @@
 (cl:defpackage :bodge-blobs-support
   (:use :cl)
   (:export register-library-directory
+           register-library-system-directory
            register-foreign-libraries
            list-registered-libraries
-           load-foreign-libraries))
-(cl:defpackage :bodge-blobs-support.library
-  (:use))
+           load-foreign-libraries
+           close-foreign-libraries))
 (cl:in-package :bodge-blobs-support)
 
 
 (defvar *libraries* nil)
 
 
-(defun register-library-directory (system &optional (subdirectory #p"lib/"))
+(defun register-library-directory (directory)
+  (pushnew directory cffi:*foreign-library-directories* :test #'equal))
+
+
+(defun register-library-system-directory (system &optional (subdirectory #p"lib/"))
   (let* ((sys-dir (asdf:component-pathname (asdf:find-system system)))
          (lib-dir (merge-pathnames subdirectory sys-dir)))
-    (pushnew lib-dir cffi:*foreign-library-directories* :test #'equal)))
+    (register-library-directory lib-dir)))
 
 
 (defun %register-library-names (libraries)
@@ -36,7 +40,7 @@
 (defun list-registered-libraries ()
   (let ((search-directories (cffi::parse-directories cffi:*foreign-library-directories*)))
     (loop for library-name in *libraries*
-       as library-path = (cffi::find-file library-name search-directories)
+       as library-path = (cffi::find-file (symbol-name library-name) search-directories)
        when library-path
        collect library-path)))
 
@@ -44,3 +48,8 @@
 (defun load-foreign-libraries ()
   (dolist (library *libraries*)
     (cffi:load-foreign-library library)))
+
+
+(defun close-foreign-libraries ()
+  (dolist (library (reverse *libraries*))
+    (cffi:close-foreign-library library)))
